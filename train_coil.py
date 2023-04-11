f3a1a0ab2f997ace8e01d1e3825ddc0151a0c86a
abhyudit309 <71165429+abhyudit309@users.noreply.github.com>
2023-04-11 13:57:08 -0700

    Add files via upload

---
+++ train_coil.py
@@ -0,0 +1,149 @@
+import numpy as np
+import tensorflow as tf
+import argparse
+from utils import *
+
+tf.config.run_functions_eagerly(True)
+
+class NN(tf.keras.Model):
+    def __init__(self, in_size, out_size):
+        super(NN, self).__init__()
+        
+        ######### Your code starts here #########
+        # We want to define and initialize the weights & biases of the CoIL network.
+        # - in_size is dim(O)
+        # - out_size is dim(A) = 2
+        # HINT: You should use either of the following for weight initialization:
+        #         - tf.keras.initializers.GlorotUniform (this is what we tried)
+        #         - tf.keras.initializers.GlorotNormal
+        #         - tf.keras.initializers.he_uniform or tf.keras.initializers.he_normal
+
+        input_layer = tf.keras.layers.Input(shape=in_size)
+        hidden_layer1 = tf.keras.layers.Dense(90, activation='tanh', kernel_initializer=tf.keras.initializers.GlorotUniform())(input_layer)
+        hidden_layer2 = tf.keras.layers.Dense(60, activation='tanh', kernel_initializer=tf.keras.initializers.GlorotUniform())(hidden_layer1)
+        hidden_layer3 = tf.keras.layers.Dense(30, activation='tanh', kernel_initializer=tf.keras.initializers.GlorotUniform())(hidden_layer2)
+        self.base_model = tf.keras.Model(inputs=input_layer, outputs=hidden_layer3)
+
+        self.output1 = tf.keras.layers.Dense(out_size)
+        self.output2 = tf.keras.layers.Dense(out_size)
+        self.output3 = tf.keras.layers.Dense(out_size)
+        
+        ########## Your code ends here ##########
+
+    def call(self, x, u):
+        x = tf.cast(x, dtype=tf.float32)
+        u = tf.cast(u, dtype=tf.int8)
+        ######### Your code starts here #########
+        # We want to perform a forward-pass of the network. Using the weights and biases, this function should give the network output for (x,u) where:
+        # - x is a (?, |O|) tensor that keeps a batch of observations
+        # - u is a (?, 1) tensor (a vector indeed) that keeps the high-level commands (goals) to denote which branch of the network to use 
+        # FYI: For the intersection scenario, u=0 means the goal is to turn left, u=1 straight, and u=2 right. 
+        # HINT 1: Looping over all data samples may not be the most computationally efficient way of doing branching
+        # HINT 2: While implementing this, we found tf.math.equal and tf.cast useful. This is not necessarily a requirement though.
+
+        ind1 = tf.cast(tf.math.equal(u, 0), tf.float32)
+        ind2 = tf.cast(tf.math.equal(u, 1), tf.float32)
+        ind3 = tf.cast(tf.math.equal(u, 2), tf.float32)
+
+        z = self.base_model(x)
+        z1 = tf.multiply(z, ind1)
+        z2 = tf.multiply(z, ind2)
+        z3 = tf.multiply(z, ind3)
+
+        y1 = tf.multiply(self.output1(z1), ind1)
+        y2 = tf.multiply(self.output2(z2), ind2)
+        y3 = tf.multiply(self.output3(z3), ind3)
+
+        return y1 + y2 + y3
+
+        ########## Your code ends here ##########
+
+
+def loss(y_est, y):
+    y = tf.cast(y, dtype=tf.float32)
+    ######### Your code starts here #########
+    # We want to compute the loss between y_est and y where
+    # - y_est is the output of the network for a batch of observations & goals,
+    # - y is the actions the expert took for the corresponding batch of observations & goals
+    # At the end your code should return the scalar loss value.
+    # HINT: Remember, you can penalize steering (0th dimension) and throttle (1st dimension) unequally
+
+    alpha = 0.99  # penalizing factor
+    steering_loss = (y_est[:, 0] - y[:, 0]) ** 2
+    throttle_loss = (y_est[:, 1] - y[:, 1]) ** 2
+    overall_loss = alpha * steering_loss + (1 - alpha) * throttle_loss
+    return tf.reduce_mean(overall_loss)
+
+    ########## Your code ends here ##########
+   
+
+def nn(data, args):
+    """
+    Trains a feedforward NN. 
+    """
+    params = {
+        'train_batch_size': 4096,
+    }
+    in_size = data['x_train'].shape[-1]
+    out_size = data['y_train'].shape[-1]
+    
+    nn_model = NN(in_size, out_size)
+    if args.restore:
+        nn_model.load_weights('./policies/' + args.scenario.lower() + '_' + args.goal.lower() + '_CoIL')
+    optimizer = tf.keras.optimizers.Adam(learning_rate=args.lr)
+
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+
+    @tf.function
+    def train_step(x, y, u):
+        ######### Your code starts here #########
+        # We want to perform a single training step (for one batch):
+        # 1. Make a forward pass through the model (note both x and u are inputs now)
+        # 2. Calculate the loss for the output of the forward pass
+        # 3. Based on the loss calculate the gradient for all weights
+        # 4. Run an optimization step on the weights.
+        # Helpful Functions: tf.GradientTape(), tf.GradientTape.gradient(), tf.keras.Optimizer.apply_gradients
+
+        with tf.GradientTape() as g:
+            g.watch(nn_model.trainable_variables)
+            y_est = nn_model.call(x, u)
+            current_loss = loss(y_est, y)
+        grad = g.gradient(current_loss, nn_model.trainable_variables)
+        optimizer.apply_gradients(zip(grad, nn_model.trainable_variables))
+
+        ########## Your code ends here ##########
+
+        train_loss(current_loss)
+
+    @tf.function
+    def train(train_data):
+        for x, y, u in train_data:
+            train_step(x, y, u)
+
+    train_data = tf.data.Dataset.from_tensor_slices((data['x_train'], data['y_train'], data['u_train'])).shuffle(100000).batch(params['train_batch_size'])
+
+    for epoch in range(args.epochs):
+        # Reset the metrics at the start of the next epoch
+        train_loss.reset_states()
+
+        train(train_data)
+
+        template = 'Epoch {}, Loss: {}'
+        print(template.format(epoch + 1, train_loss.result()))
+    nn_model.save_weights('./policies/' + args.scenario.lower() + '_' + args.goal.lower() + '_CoIL')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--scenario', type=str, help="intersection, circularroad", default="intersection")
+    parser.add_argument("--epochs", type=int, help="number of epochs for training", default=1000)
+    parser.add_argument("--lr", type=float, help="learning rate for Adam optimizer", default=5e-3)
+    parser.add_argument("--restore", action="store_true", default=False)
+    args = parser.parse_args()
+    args.goal = 'all'
+    
+    maybe_makedirs("./policies")
+    
+    data = load_data(args)
+
+    nn(data, args)
---
+++ train_ildist.py
@@ -0,0 +1,137 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow_probability import distributions as tfd
+import argparse
+from utils import *
+
+tf.config.run_functions_eagerly(True)
+
+class NN(tf.keras.Model):
+    def __init__(self, in_size, out_size):
+        super(NN, self).__init__()
+        
+        ######### Your code starts here #########
+        # We want to define and initialize the weights & biases of the neural network.
+        # - in_size is dim(O)
+        # - out_size is dim(A) = 2
+        # IMPORTANT: out_size is still 2 in this case, because the action space is 2-dimensional. But your network will output some other size as it is outputing a distribution!
+        # HINT: You should use either of the following for weight initialization:
+        #         - tf.keras.initializers.GlorotUniform (this is what we tried)
+        #         - tf.keras.initializers.GlorotNormal
+        #         - tf.keras.initializers.he_uniform or tf.keras.initializers.he_normal
+
+        output_size = out_size ** 2 + out_size
+        input_layer = tf.keras.layers.Input(shape=in_size)
+        hidden_layer1 = tf.keras.layers.Dense(90, activation='tanh', kernel_initializer=tf.keras.initializers.GlorotUniform())(input_layer)
+        hidden_layer2 = tf.keras.layers.Dense(45, activation='tanh', kernel_initializer=tf.keras.initializers.GlorotUniform())(hidden_layer1)
+        output = tf.keras.layers.Dense(output_size)(hidden_layer2)
+        self.model = tf.keras.Model(inputs=input_layer, outputs=output)
+        
+        ########## Your code ends here ##########
+
+    def call(self, x):
+        x = tf.cast(x, dtype=tf.float32)
+        ######### Your code starts here #########
+        # We want to perform a forward-pass of the network. Using the weights and biases, this function should give the network output for x where:
+        # x is a (?, |O|) tensor that keeps a batch of observations
+        # IMPORTANT: First two columns of the output tensor must correspond to the mean vector!
+        
+        return self.model(x)
+        
+        ########## Your code ends here ##########
+
+
+   
+def loss(y_est, y):
+    y = tf.cast(y, dtype=tf.float32)
+    ######### Your code starts here #########
+    # We want to compute the negative log-likelihood loss between y_est and y where
+    # - y_est is the output of the network for a batch of observations,
+    # - y is the actions the expert took for the corresponding batch of observations
+    # At the end your code should return the scalar loss value.
+    # HINT: You may find the classes of tensorflow_probability.distributions (imported as tfd) useful.
+    #       In particular, you can use MultivariateNormalFullCovariance or MultivariateNormalTriL, but they are not the only way.
+
+    epsilon = 0.001
+    N, action_dim = y.shape
+    mu = y_est[:, 0:action_dim]
+    A = tf.reshape(y_est[:, action_dim:], (N, action_dim, action_dim))
+    cov = tf.matmul(A, tf.transpose(A, perm=[0, 2, 1])) + epsilon * tf.eye(action_dim)
+    mvn = tfd.MultivariateNormalTriL(loc=mu, scale_tril=tf.linalg.cholesky(cov))
+    l = tf.reduce_sum(mvn.log_prob(y))
+    return -l / N
+    
+    ########## Your code ends here ##########
+
+
+def nn(data, args):
+    """
+    Trains a feedforward NN. 
+    """
+    params = {
+        'train_batch_size': 4096*32,
+    }
+    in_size = data['x_train'].shape[-1]
+    out_size = data['y_train'].shape[-1]
+    
+    nn_model = NN(in_size, out_size)
+    if args.restore:
+        nn_model.load_weights('./policies/' + args.scenario.lower() + '_' + args.goal.lower() + '_ILDIST')
+    optimizer = tf.keras.optimizers.Adam(learning_rate=args.lr)
+
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+
+    @tf.function
+    def train_step(x, y):
+        ######### Your code starts here #########
+        # We want to perform a single training step (for one batch):
+        # 1. Make a forward pass through the model
+        # 2. Calculate the loss for the output of the forward pass
+        # 3. Based on the loss calculate the gradient for all weights
+        # 4. Run an optimization step on the weights.
+        # Helpful Functions: tf.GradientTape(), tf.GradientTape.gradient(), tf.keras.Optimizer.apply_gradients
+
+        with tf.GradientTape() as g:
+            g.watch(nn_model.trainable_variables)
+            y_est = nn_model.call(x)
+            current_loss = loss(y_est, y)
+        grad = g.gradient(current_loss, nn_model.trainable_variables)
+        optimizer.apply_gradients(zip(grad, nn_model.trainable_variables))
+       
+        ########## Your code ends here ##########
+
+        train_loss(current_loss)
+
+    @tf.function
+    def train(train_data):
+        for x, y in train_data:
+            train_step(x, y)
+
+
+    train_data = tf.data.Dataset.from_tensor_slices((data['x_train'], data['y_train'])).shuffle(100000).batch(params['train_batch_size'])
+
+    for epoch in range(args.epochs):
+        # Reset the metrics at the start of the next epoch
+        train_loss.reset_states()
+
+        train(train_data)
+
+        template = 'Epoch {}, Loss: {}'
+        print(template.format(epoch + 1, train_loss.result()))
+    nn_model.save_weights('./policies/' + args.scenario.lower() + '_' + args.goal.lower() + '_ILDIST')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--goal', type=str, help="left, straight, right, inner, outer, all", default="all")
+    parser.add_argument('--scenario', type=str, help="intersection, circularroad", default="intersection")
+    parser.add_argument("--epochs", type=int, help="number of epochs for training", default=1000)
+    parser.add_argument("--lr", type=float, help="learning rate for Adam optimizer", default=1e-3)
+    parser.add_argument("--restore", action="store_true", default=False)
+    args = parser.parse_args()
+    
+    maybe_makedirs("./policies")
+    
+    data = load_data(args)
+
+    nn(data, args)
